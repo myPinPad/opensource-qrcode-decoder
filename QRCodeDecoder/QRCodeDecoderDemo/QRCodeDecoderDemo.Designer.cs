e5f5111804c2bedb9f0c8ce92f6f791de35206dd
Uzi Granot <ugranot@gmail.com>
2019-05-16 16:40:15 -0400

    Add files via upload

---
+++ QRCodeDecoder/QRCodeDecoderDemo/QRCodeDecoderDemo.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace QRCodeDecoderDemo
+{
+	partial class QRCodeDecoderDemo
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(QRCodeDecoderDemo));
+			this.ImageFileLabel = new System.Windows.Forms.Label();
+			this.DecodedDataLabel = new System.Windows.Forms.Label();
+			this.LoadImageButton = new System.Windows.Forms.Button();
+			this.HeaderLabel = new System.Windows.Forms.Label();
+			this.DataTextBox = new System.Windows.Forms.TextBox();
+			this.SuspendLayout();
+			// 
+			// ImageFileLabel
+			// 
+			this.ImageFileLabel.BackColor = System.Drawing.SystemColors.Info;
+			this.ImageFileLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.ImageFileLabel.Location = new System.Drawing.Point(180, 527);
+			this.ImageFileLabel.Name = "ImageFileLabel";
+			this.ImageFileLabel.Size = new System.Drawing.Size(407, 20);
+			this.ImageFileLabel.TabIndex = 12;
+			this.ImageFileLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// DecodedDataLabel
+			// 
+			this.DecodedDataLabel.AutoSize = true;
+			this.DecodedDataLabel.Location = new System.Drawing.Point(7, 390);
+			this.DecodedDataLabel.Name = "DecodedDataLabel";
+			this.DecodedDataLabel.Size = new System.Drawing.Size(88, 16);
+			this.DecodedDataLabel.TabIndex = 9;
+			this.DecodedDataLabel.Text = "Decoded data";
+			// 
+			// LoadImageButton
+			// 
+			this.LoadImageButton.Location = new System.Drawing.Point(8, 519);
+			this.LoadImageButton.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+			this.LoadImageButton.Name = "LoadImageButton";
+			this.LoadImageButton.Size = new System.Drawing.Size(164, 38);
+			this.LoadImageButton.TabIndex = 11;
+			this.LoadImageButton.Text = "Load QR Code Image";
+			this.LoadImageButton.UseVisualStyleBackColor = true;
+			this.LoadImageButton.Click += new System.EventHandler(this.OnLoadImage);
+			// 
+			// HeaderLabel
+			// 
+			this.HeaderLabel.BackColor = System.Drawing.SystemColors.ActiveCaption;
+			this.HeaderLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+			this.HeaderLabel.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.HeaderLabel.Location = new System.Drawing.Point(234, 8);
+			this.HeaderLabel.Name = "HeaderLabel";
+			this.HeaderLabel.Size = new System.Drawing.Size(180, 32);
+			this.HeaderLabel.TabIndex = 0;
+			this.HeaderLabel.Text = "QR Code Decoder";
+			this.HeaderLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// DataTextBox
+			// 
+			this.DataTextBox.AcceptsReturn = true;
+			this.DataTextBox.BackColor = System.Drawing.SystemColors.Info;
+			this.DataTextBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.DataTextBox.Cursor = System.Windows.Forms.Cursors.Default;
+			this.DataTextBox.Location = new System.Drawing.Point(8, 410);
+			this.DataTextBox.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+			this.DataTextBox.Multiline = true;
+			this.DataTextBox.Name = "DataTextBox";
+			this.DataTextBox.ReadOnly = true;
+			this.DataTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+			this.DataTextBox.Size = new System.Drawing.Size(572, 101);
+			this.DataTextBox.TabIndex = 10;
+			this.DataTextBox.TabStop = false;
+			this.DataTextBox.Text = "\r\n";
+			// 
+			// QRCodeDecoderDemo
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+			this.ClientSize = new System.Drawing.Size(624, 561);
+			this.Controls.Add(this.ImageFileLabel);
+			this.Controls.Add(this.LoadImageButton);
+			this.Controls.Add(this.DataTextBox);
+			this.Controls.Add(this.DecodedDataLabel);
+			this.Controls.Add(this.HeaderLabel);
+			this.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+			this.MinimumSize = new System.Drawing.Size(640, 600);
+			this.Name = "QRCodeDecoderDemo";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "Form1";
+			this.Load += new System.EventHandler(this.OnLoad);
+			this.Paint += new System.Windows.Forms.PaintEventHandler(this.OnPaint);
+			this.Resize += new System.EventHandler(this.OnResize);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+		private System.Windows.Forms.Label ImageFileLabel;
+		private System.Windows.Forms.Label DecodedDataLabel;
+		private System.Windows.Forms.Button LoadImageButton;
+		private System.Windows.Forms.Label HeaderLabel;
+		private System.Windows.Forms.TextBox DataTextBox;
+	}
+}
+
---
+++ QRCodeDecoder/QRCodeVideoDecoder/QRCodeVideoDecoder.cs
@@ -0,0 +1,304 @@
+﻿/////////////////////////////////////////////////////////////////////
+//
+//	QR Code Library
+//
+//	QR Code video camera decoder.
+//
+//	Author: Uzi Granot
+//	Original Version: 1.0
+//	Date: June 30, 2018
+//	Copyright (C) 2018-2019 Uzi Granot. All Rights Reserved
+//	For full version history please look at QRDecoder.cs
+//
+//	QR Code Library C# class library and the attached test/demo
+//  applications are free software.
+//	Software developed by this author is licensed under CPOL 1.02.
+//	Some portions of the QRCodeVideoDecoder are licensed under GNU Lesser
+//	General Public License v3.0.
+//
+//	The solution is made of 3 projects:
+//	1. QRCodeDecoderLibrary: QR code decoding.
+//	3. QRCodeDecoderDemo: Decode QR code image files.
+//	4. QRCodeVideoDecoder: Decode QR code using web camera.
+//		This demo program is using some of the source modules of
+//		Camera_Net project published at CodeProject.com:
+//		https://www.codeproject.com/Articles/671407/Camera_Net-Library
+//		and at GitHub: https://github.com/free5lot/Camera_Net.
+//		This project is based on DirectShowLib.
+//		http://sourceforge.net/projects/directshownet/
+//		This project includes a modified subset of the source modules.
+//
+//	The main points of CPOL 1.02 subject to the terms of the License are:
+//
+//	Source Code and Executable Files can be used in commercial applications;
+//	Source Code and Executable Files can be redistributed; and
+//	Source Code can be modified to create derivative works.
+//	No claim of suitability, guarantee, or any warranty whatsoever is
+//	provided. The software is provided "as-is".
+//	The Article accompanying the Work may not be distributed or republished
+//	without the Author's consent
+//
+//	For version history please refer to QRDecoder.cs
+/////////////////////////////////////////////////////////////////////
+
+using DirectShowLib;
+using QRCodeDecoderLibrary;
+using System;
+using System.Drawing;
+using System.IO;
+using System.Runtime.InteropServices.ComTypes;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Text;
+
+namespace QRCodeVideoDecoder
+{
+/// <summary>
+/// QR Code camera captuer using Direct Show Library
+/// </summary>
+public partial class QRCodeVideoDecoder : Form
+	{
+	private FrameSize FrameSize = new FrameSize(640, 480);
+	private Camera VideoCamera;
+	private Timer QRCodeTimer;
+	private QRDecoder Decoder;
+
+	/// <summary>
+	/// Constructor
+	/// </summary>
+	public QRCodeVideoDecoder()
+		{
+		InitializeComponent();
+		return;
+		}
+
+	/// <summary>
+	/// Program initialization
+	/// </summary>
+	/// <param name="sender">Sender</param>
+	/// <param name="e">Event arguments</param>
+	private void OnLoad(object sender, EventArgs e)
+		{
+		// program title
+		Text = "QRCodeVideoDecoder - " + QRDecoder.VersionNumber + " \u00a9 2013-2018 Uzi Granot. All rights reserved.";
+
+		#if DEBUG
+		// current directory
+		string CurDir = Environment.CurrentDirectory;
+		string WorkDir = CurDir.Replace("bin\\Debug", "Work");
+		if(WorkDir != CurDir && Directory.Exists(WorkDir)) Environment.CurrentDirectory = WorkDir;
+
+		// open trace file
+		QRCodeTrace.Open("QRCodeVideoDecoderTrace.txt");
+		QRCodeTrace.Write(Text);
+		#endif
+
+		// disable reset button
+		ResetButton.Enabled = false;
+		GoToUriButton.Enabled = false;
+
+		// get an array of web camera devices
+		DsDevice[] CameraDevices = DsDevice.GetDevicesOfCat(FilterCategory.VideoInputDevice);
+
+		// make sure at least one is available
+		if(CameraDevices == null || CameraDevices.Length == 0)
+			{
+			MessageBox.Show("No video cameras in this computer");
+			Close();
+			return;
+			}
+
+		// select the first camera
+		DsDevice CameraDevice = CameraDevices[0];
+
+		// Device moniker
+		IMoniker CameraMoniker = CameraDevice.Moniker;
+
+		// get a list of frame sizes available
+		FrameSize[] FrameSizes = Camera.GetFrameSizeList(CameraMoniker);
+
+		// make sure there is at least one frame size
+		if(FrameSizes == null || FrameSizes.Length == 0)
+			{
+			MessageBox.Show("No video cameras in this computer");
+			Close();
+			return;
+			}
+
+		// test if our frame size is available
+		int Index;
+		for(Index = 0; Index < FrameSizes.Length &&
+			(FrameSizes[Index].Width != FrameSize.Width || FrameSizes[Index].Height != FrameSize.Height); Index++);
+
+		// select first frame size
+		if(Index == FrameSizes.Length) FrameSize = FrameSizes[0];
+
+		// Set selected camera to camera control with default frame size
+		// Create camera object
+		VideoCamera = new Camera(PreviewPanel, CameraMoniker, FrameSize);
+
+		// create QR code decoder
+		Decoder = new QRDecoder();
+
+		// resize window
+		OnResize(sender, e);
+
+		// create timer
+		QRCodeTimer = new Timer();
+		QRCodeTimer.Interval = 200;
+		QRCodeTimer.Tick += QRCodeTimer_Tick;
+		QRCodeTimer.Enabled = true;
+		return;
+		}
+
+	private void QRCodeTimer_Tick(object sender, EventArgs e)
+		{
+		QRCodeTimer.Enabled = false;
+		Bitmap QRCodeImage;
+		try
+			{
+			QRCodeImage = VideoCamera.SnapshotSourceImage();
+
+			// trace
+			#if DEBUG
+			QRCodeTrace.Format("Image width: {0}, Height: {1}", QRCodeImage.Width, QRCodeImage.Height);
+			#endif
+			}
+
+		catch (Exception EX)
+			{
+			DataTextBox.Text = "Decode exception.\r\n" + EX.Message;
+			QRCodeTimer.Enabled = true;
+			return;
+			}
+
+		// decode image
+		byte[][] DataByteArray = Decoder.ImageDecoder(QRCodeImage);
+		string Text = QRCodeResult(DataByteArray);
+
+		// dispose bitmap
+		QRCodeImage.Dispose();
+
+		// we have no QR code
+		if(Text.Length == 0)
+			{
+			QRCodeTimer.Enabled = true;
+			return;
+			}
+
+		VideoCamera.PauseGraph();
+
+		DataTextBox.Text = Text;
+		ResetButton.Enabled = true;
+		if(IsValidUri(DataTextBox.Text)) GoToUriButton.Enabled = true;
+		return;
+		}
+
+	/// <summary>
+	/// Format result for display
+	/// </summary>
+	/// <param name="DataByteArray"></param>
+	/// <returns></returns>
+	private static string QRCodeResult
+			(
+			byte[][] DataByteArray
+			)
+		{
+		// no QR code
+		if(DataByteArray == null) return string.Empty;
+
+		// image has one QR code
+		if(DataByteArray.Length == 1) return QRDecoder.ByteArrayToStr(DataByteArray[0]);
+
+		// image has more than one QR code
+		StringBuilder Str = new StringBuilder();
+		for(int Index = 0; Index < DataByteArray.Length; Index++)
+			{
+			if(Index != 0) Str.Append("\r\n");
+			Str.AppendFormat("QR Code {0}\r\n", Index + 1);
+			Str.Append(QRDecoder.ByteArrayToStr(DataByteArray[Index]));
+			}
+		return Str.ToString();
+		}
+
+	private static bool IsValidUri(string Uri)
+		{
+		if(!System.Uri.IsWellFormedUriString(Uri, UriKind.Absolute)) return false;
+
+		if(!System.Uri.TryCreate(Uri, UriKind.Absolute, out Uri TempUri)) return false;
+
+		return TempUri.Scheme == System.Uri.UriSchemeHttp || TempUri.Scheme == System.Uri.UriSchemeHttps;
+		}
+
+	/// <summary>
+	/// Reset button was pressed
+	/// </summary>
+	/// <param name="sender">Sender</param>
+	/// <param name="e">Event arguments</param>
+	private void OnResetButton(object sender, EventArgs e)
+		{
+		VideoCamera.RunGraph();
+		QRCodeTimer.Enabled = true;
+		ResetButton.Enabled = false;
+		GoToUriButton.Enabled = false;
+		DataTextBox.Text = string.Empty;
+		return;
+		}
+
+	/// <summary>
+	/// 
+	/// </summary>
+	/// <param name="sender">Sender</param>
+	/// <param name="e">Event arguments</param>
+	private void OnGoToUri(object sender, EventArgs e)
+		{
+		Process.Start(DataTextBox.Text);
+		return;
+		}
+
+	/// <summary>
+	/// Resize window
+	/// </summary>
+	/// <param name="sender">Sender</param>
+	/// <param name="e">Event arguments</param>
+	private void OnResize(object sender, EventArgs e)
+		{
+		// minimize
+		if(ClientSize.Width == 0) return;
+
+		// put reset button at bottom center
+		ResetButton.Left = ClientSize.Width / 2 - ResetButton.Width - 8;
+		ResetButton.Top = ClientSize.Height - ResetButton.Height - 8;
+		GoToUriButton.Left = ResetButton.Right + 16;
+		GoToUriButton.Top = ResetButton.Top;
+
+		// data text box
+		DataTextBox.Top = ResetButton.Top - DataTextBox.Height - 8;
+		DataTextBox.Width = ClientSize.Width - 8 - SystemInformation.VerticalScrollBarWidth;
+
+		// decoded data label
+		DecodedDataLabel.Top = DataTextBox.Top - DecodedDataLabel.Height - 4;
+
+		// preview area
+		int AreaWidth = ClientSize.Width - 4;
+		int AreaHeight = DecodedDataLabel.Top - 4;
+		if(AreaHeight > FrameSize.Height * AreaWidth / FrameSize.Width)
+			AreaHeight = FrameSize.Height * AreaWidth / FrameSize.Width;
+		else
+			AreaWidth = FrameSize.Width * AreaHeight / FrameSize.Height;
+
+		// preview panel
+		PreviewPanel.Left = (ClientSize.Width - AreaWidth) / 2;
+		PreviewPanel.Top = (DecodedDataLabel.Top - 4 - AreaHeight) / 2;
+		PreviewPanel.Width = AreaWidth;
+		PreviewPanel.Height = AreaHeight;
+		return;
+		}
+
+	private void OnClosing(object sender, FormClosingEventArgs e)
+		{
+		if(VideoCamera != null) VideoCamera.Dispose();
+		return;
+		}
+	}
+}
