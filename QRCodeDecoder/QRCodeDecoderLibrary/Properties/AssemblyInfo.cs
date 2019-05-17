81d9b1a3ddddf5cf13e6fa2da63423e5355a37c2
Uzi Granot <ugranot@gmail.com>
2019-05-17 11:51:17 -0400

    Add files via upload

--- QRCodeDecoder/QRCodeDecoderLibrary/Properties/AssemblyInfo.cs
+++ QRCodeDecoder/QRCodeDecoderLibrary/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("QRCodeEncoderDecoderLibrary")]
-[assembly: AssemblyDescription("QR Code encoder decoder .NET library")]
+[assembly: AssemblyTitle("QRCodeDecoderLibrary")]
+[assembly: AssemblyDescription("QR Code decoder .NET library")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Author: Uzi Granot")]
-[assembly: AssemblyProduct("QRCodeEncoderDecoderLibrary")]
-[assembly: AssemblyCopyright("Copyright © Uzi Granot 2013-2018")]
+[assembly: AssemblyProduct("QRCodeDecoderLibrary")]
+[assembly: AssemblyCopyright("Copyright © 2018-2019 Uzi Granot. All rights reserved.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.1.0")]
-[assembly: AssemblyFileVersion("1.1.1.0")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
